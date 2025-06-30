e920d0c0e00128d3624ebbf947c8a984516e6afe
ghost <ghost@devmail.com>
2025-06-30 20:19:52 +0530

    Create Hero section and add images

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const images = [
+  "./assets/Bedroom/image-7.png",
+  "./assets/Manufacturing/image-10.png",
+  "./assets/HomeFurnitures/image-10.png",
+  "./assets/Kitchen/image-6.png",
+  "./assets/Office/image-5.png",
+  "./assets/HomeFurnitures/image-9.png",
+  "./assets/Manufacturing/image-9.png",
+];
+
+let index = 0;
+const currentImg = document.getElementById("current");
+const nextImg = document.getElementById("next");
+
+// Set initial image
+currentImg.src = images[index];
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+
+  nextImg.src = images[index];
+  nextImg.classList.remove("hidden");
+  nextImg.classList.add("slide-in");
+  currentImg.classList.add("slide-out");
+
+  setTimeout(() => {
+    // Swap roles
+    currentImg.src = nextImg.src;
+    currentImg.classList.remove("slide-out");
+    nextImg.classList.remove("slide-in");
+    nextImg.classList.add("hidden");
+  }, 1000); // matches animation duration
+}, 5000);
