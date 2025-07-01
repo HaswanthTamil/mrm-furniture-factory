a5bea7a677d640ad9c041b985b37cb2990b12aa1
ghost <ghost@devmail.com>
2025-07-01 10:07:02 +0530

    Test new import method

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,33 +1,38 @@
-const images = [
-  "/public/assets/Bedroom/Image-7.png",
-  "/assets/Manufacturing/Image-10.png",
-  "/assets/HomeFurnitures/Image-10.png",
-  "/assets/Kitchen/Image-6.png",
-  "/assets/Office/Image-5.png",
-  "/assets/HomeFurnitures/Image-9.png",
-  "/assets/Manufacturing/Image-9.png",
-];
+const prefix = location.hostname === "localhost" ? "" : "."
 
-let index = 0;
-const currentImg = document.getElementById("current");
-const nextImg = document.getElementById("next");
+const images = [
+  `${prefix}/assets/Bedroom/Image-7.png`,
+  `${prefix}/assets/Manufacturing/Image-10.png`,
+  `${prefix}/assets/HomeFurnitures/Image-10.png`,
+  `${prefix}/assets/Kitchen/Image-6.png`,
+  `${prefix}/assets/Office/Image-5.png`,
+  `${prefix}/assets/HomeFurnitures/Image-9.png`,
+  `${prefix}/assets/Manufacturing/Image-9.png`,
+]
+
+let index = 0
+const currentImg = document.getElementById("current")
+const nextImg = document.getElementById("next")
 
 // Set initial image
-currentImg.src = images[index];
+currentImg.src = images[index]
 
 setInterval(() => {
-  index = (index + 1) % images.length;
+  index = (index + 1) % images.length
 
-  nextImg.src = images[index];
-  nextImg.classList.remove("hidden");
-  nextImg.classList.add("slide-in");
-  currentImg.classList.add("slide-out");
+  nextImg.src = images[index]
+  nextImg.classList.remove("hidden")
+  nextImg.classList.add("slide-in")
+  currentImg.classList.add("slide-out")
 
   setTimeout(() => {
     // Swap roles
-    currentImg.src = nextImg.src;
-    currentImg.classList.remove("slide-out");
-    nextImg.classList.remove("slide-in");
-    nextImg.classList.add("hidden");
-  }, 1000); // matches animation duration
-}, 5000);
+    currentImg.src = nextImg.src
+    currentImg.classList.remove("slide-out")
+    nextImg.classList.remove("slide-in")
+    nextImg.classList.add("hidden")
+  }, 1000) // matches animation duration
+}, 5000)
+
+const texture_hero = document.getElementById("texture_hero")
+texture_hero.src = `${prefix}/assets/Office/Image-4.png`
